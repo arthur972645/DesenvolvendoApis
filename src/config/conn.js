06a3be4d4d9a4c287582b2b04f7280d4184030cd
arthur972645 <138785050+arthur972645@users.noreply.github.com>
2024-08-12 00:13:51 -0300

    Add files via upload

--- src/config/conn.js
+++ src/config/conn.js
@@ -1,20 +1,19 @@
-import mysql from "mysql2";
+import mysql from "mysql2"
 
 const conn = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.MYSQL_HOST,
+  host: process.env.MTSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
-});
+  port: process.env.MYSQL_PORTM,
+})
 
 conn.query("SELECT 1 + 1 AS solution", (err, result, fields) => {
-  if (err) {
-    console.error(err);
-    return;
+  if(err){
+    console.error(err)
+    return
   }
-  console.log("The solution is: ", result[0].solution);
-});
-
-export default conn;
+  console.log("The solcution is ", result[0].solution)
+})
+ export default conn
